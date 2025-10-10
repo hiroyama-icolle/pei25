dbe78ea3a6bf64bf1d9b53f74cc57d1d982c90d9
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2025-10-10 03:02:27 +0000

    1010 kadai

---
+++ m1_q4_18kadai.py
@@ -0,0 +1,11 @@
+phrase = 'PythonProgramming'
+list_p = [] # 空のリストを作成
+for p in phrase: # 繰り返しで位置文字ずつ検査
+	if p not in list_p: # pがlist_pにまだ含まれていなければ
+		list_p.append(p) # list_pにpを追加
+print(len(phrase) - len(list_p)) # 17-11=6つまり重複した文字の数
+
+for p in list_p: # コメントで説明してください
+    print(p, end="") #説明してくだし
+
+print() # 改行のみ出力
